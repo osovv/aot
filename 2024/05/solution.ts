50813b1f20a53b83d0b3d1c70cd35dbe7e4c6d9b
Aleksey Chendemerov <chendemerov.aleksey@gmail.com>
2024-12-05 19:30:16 +0300

    2024-12-05 add solution

--- 2024/05/solution.ts
+++ 2024/05/solution.ts
@@ -1,4 +1,4 @@
-const createRoute = (author: string, route: unknown) => {
+const createRoute = <T>(author: string, route: T) => {
   console.log(`[createRoute] route created by ${author} at ${Date.now()}`);
   return route;
 };
